ed59721ce820a5b61fdca011bc59611aecc0d230
Alaref-Abushaala <58532122+alaref-codes@users.noreply.github.com>
2020-11-29 23:42:17 +0200

    Update weather.py

--- weather.py
+++ weather.py
@@ -6,9 +6,6 @@ root = Tk()
 root.title("Weather App")
 root.geometry("450x40")
 
-
-# https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7CCB228B-8EA4-464C-909F-8B1A683C56F4
-
 try:
     api_request = requests.get("https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=20002&distance=5&API_KEY=7CCB228B-8EA4-464C-909F-8B1A683C56F4")
     api = json.loads(api_request.content)
